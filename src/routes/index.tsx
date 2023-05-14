8142433527099188ad6a53f323b063afa6615847
Jeki Maulana <jekibus@gmail.com>
2023-05-14 21:09:03 +0700

    Create homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,38 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import About from '~/components/about';
+import Footer from '~/components/footer';
+import Cat from '~/components/footer/cat';
+import Header from '~/components/header';
+import HeroHome from '~/components/hero/home';
+import Faq from '~/components/home/faq';
+import Features from '~/components/home/features';
+import Programs from '~/components/home/programs';
+import Testimonial from '~/components/home/testimonial';
 
 export default component$(() => {
   return (
     <>
-      <div class="flex items-center justify-center h-screen">
-        <h1 class="mr-3">Hi 👋</h1>
-        <p>
-          Can't wait to see what you build with <span class="bg-green-200 px-1">Edubase</span>
-          <br />
-          Happy coding.
-        </p>
-      </div>
+    <div class='bg-gradient-to-b from-purple-25 to-purple-50'>
+      <Header />
+      <HeroHome />
+    </div>
+
+    <div class='w-full h-40 sm:h-48 xl:h-52 bg-gradient-to-b from-purple-50 to-yellow-100'></div>
+
+    <Features />
+
+    <About />
+
+    <Programs />
+
+    <Testimonial />
+
+    <Faq />
+
+    <Cat />
+
+    <Footer />
     </>
   );
 });
