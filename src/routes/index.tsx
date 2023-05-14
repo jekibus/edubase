3b3c627846708bb29d5c15cc9010348fbbb93558
Jeki Maulana <jekibus@gmail.com>
2023-05-14 12:35:27 +0700

    Ini content

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,24 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <div class="flex items-center justify-center h-screen">
+        <h1 class="mr-3">Hi 👋</h1>
+        <p>
+          Can't wait to see what you build with <span class="bg-green-200 px-1">Edubase</span>
+          <br />
+          Happy coding.
+        </p>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Welcome to Edubase',
   meta: [
     {
       name: 'description',
-      content: 'Qwik site description',
+      content: 'The best education website platform for your favorite schoolsQwik site description',
     },
   ],
 };
